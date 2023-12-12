aa8761b37cfcf3ae8e021d1513b65be220cabd43
Eugene Safonov <eugene.safonov@ceridian.com>
2023-12-12 16:46:08 +0100

    test workflow

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello World!')
+import { getInput } from "@actions/core";
+import { context } from "@actions/github";
+
+function greet(name: string, repoUrl: string) {
+    console.log(`'Hello ${name}! You are running a GH Action in ${repoUrl}'`);
+}
+
+type GithubContext = typeof context;
+function getRepoUrl({repo, serverUrl}: GithubContext): string {
+    return `${serverUrl}/${repo.owner}/${repo.repo}`;
+}
+
+const inputName = getInput("name");
+greet(inputName, getRepoUrl(context));
